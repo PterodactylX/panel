95c2403429af79d29fb5e8a48349b8c4493ec368
Angelillo15 <contact@angelillo15.es>
2024-07-27 18:17:11 +0200

    feat: Support shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
-const colors = require('tailwindcss/colors');
+import colors from 'tailwindcss/colors';
 
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
     darkMode: ['class'],
     content: ['./resources/scripts/**/*.{js,ts,tsx}'],
+    prefix: '',
     theme: {
         container: {
             center: true,
@@ -12,68 +14,88 @@ module.exports = {
             },
         },
         extend: {
-            border: 'hsl(var(--border))',
-            input: 'hsl(var(--input))',
-            ring: 'hsl(var(--ring))',
-            background: 'hsl(var(--background))',
-            foreground: 'hsl(var(--foreground))',
-            primary: {
-                DEFAULT: 'hsl(var(--primary))',
-                foreground: 'hsl(var(--primary-foreground))',
-            },
-            secondary: {
-                DEFAULT: 'hsl(var(--secondary))',
-                foreground: 'hsl(var(--secondary-foreground))',
-            },
-            destructive: {
-                DEFAULT: 'hsl(var(--destructive))',
-                foreground: 'hsl(var(--destructive-foreground))',
-            },
-            muted: {
-                DEFAULT: 'hsl(var(--muted))',
-                foreground: 'hsl(var(--muted-foreground))',
-            },
-            accent: {
-                DEFAULT: 'hsl(var(--accent))',
-                foreground: 'hsl(var(--accent-foreground))',
-            },
-            popover: {
-                DEFAULT: 'hsl(var(--popover))',
-                foreground: 'hsl(var(--popover-foreground))',
-            },
-            card: {
-                DEFAULT: 'hsl(var(--card))',
-                foreground: 'hsl(var(--card-foreground))',
-            },
-            fontFamily: {
-                header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
-            },
             colors: {
                 black: '#131a20',
                 slate: colors.zinc,
                 // "primary" and "neutral" are deprecated, prefer the use of "blue" and "gray"
                 // in new code.
-                primary: colors.blue,
                 gray: colors.zinc,
                 neutral: colors.zinc,
                 cyan: colors.cyan,
+                zinc: colors.zinc,
+                primary: colors.zinc,
+                yellow: colors.yellow,
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
+                keyframes: {
+                    'accordion-down': {
+                        from: { height: '0' },
+                        to: { height: 'var(--radix-accordion-content-height)' },
+                    },
+                    'accordion-up': {
+                        from: { height: 'var(--radix-accordion-content-height)' },
+                        to: { height: '0' },
+                    },
+                },
+                animation: {
+                    'accordion-down': 'accordion-down 0.2s ease-out',
+                    'accordion-up': 'accordion-up 0.2s ease-out',
+                },
             },
-            fontSize: {
-                '2xs': '0.625rem',
+            fontFamily: {
+                header: ['"IBM Plex Sans"', '"Roboto"', 'system-ui', 'sans-serif'],
             },
-            transitionDuration: {
-                250: '250ms',
-            },
-            borderColor: theme => ({
-                default: theme('colors.neutral.400', 'currentColor'),
-            }),
+        },
+        fontSize: {
+            '2xs': '0.625rem',
+            xs: '0.75rem',
+            sm: '0.875rem',
+            base: '1rem',
+            lg: '1.125rem',
+            xl: '1.25rem',
+            '2xl': '1.5rem',
+            '3xl': '1.875rem',
+            '4xl': '2.25rem',
+            '5xl': '3rem',
+            '6xl': '4rem',
+            '7xl': '5rem',
+            '8xl': '6rem',
         },
     },
     plugins: [
-        require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/forms')({
-            strategy: 'class',
-        }),
+        require('tailwindcss-animate'),
+        require('@tailwindcss/line-clamp')
     ],
     borderRadius: {
         lg: 'var(--radius)',
