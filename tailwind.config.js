e074798e2a234f25b536ac539558dea3931a8778
Angelillo15 <contact@angelillo15.es>
2024-07-28 01:53:55 +0200

    fix: border radius and keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -92,25 +92,26 @@ export default {
             '7xl': '5rem',
             '8xl': '6rem',
         },
+        borderRadius: {
+            full: 'var(--radius)',
+            lg: 'var(--radius)',
+            md: 'calc(var(--radius) - 2px)',
+            sm: 'calc(var(--radius) - 4px)',
+        },
+        keyframes: {
+            'accordion-down': {
+                from: { height: '0' },
+                to: { height: 'var(--radix-accordion-content-height)' },
+            },
+            'accordion-up': {
+                from: { height: 'var(--radix-accordion-content-height)' },
+                to: { height: '0' },
+            },
+        },
     },
     plugins: [
         require('tailwindcss-animate')
     ],
-    borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-    },
-    keyframes: {
-        'accordion-down': {
-            from: { height: '0' },
-            to: { height: 'var(--radix-accordion-content-height)' },
-        },
-        'accordion-up': {
-            from: { height: 'var(--radix-accordion-content-height)' },
-            to: { height: '0' },
-        },
-    },
     animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
