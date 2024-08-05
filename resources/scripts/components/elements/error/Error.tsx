8042ddf62591fb7d3d6426ca33b733c3502cc68c
Angelillo15 <contact@angelillo15.es>
2024-08-05 11:03:29 +0200

    Add a new error mode with react children support

--- resources/scripts/components/elements/error/Error.tsx
+++ resources/scripts/components/elements/error/Error.tsx
@@ -3,6 +3,7 @@ import { AlertCircle } from 'lucide-react';
 
 interface Props {
     laravelError?: LaravelError;
+    children?: React.ReactNode;
 }
 
 export interface LaravelError {
@@ -15,24 +16,34 @@ export interface Error {
     detail: string;
 }
 
-const Error: React.FC<Props> = ({ laravelError }) => {
-    if (!laravelError) {
-        return null;
-    }
-
+export const GeneralError = ({ children }: { children: React.ReactNode }) => {
     return (
         <Alert variant="destructive">
             <AlertCircle className="h-4 w-4" />
             <AlertTitle>An error has been ocurred</AlertTitle>
-            <AlertDescription>
+            <AlertDescription>{children}</AlertDescription>
+        </Alert>
+    );
+};
+
+const Error = ({ laravelError, children }: Props) => {
+    if (laravelError && laravelError.errors && laravelError.errors.length !== 0) {
+        return (
+            <GeneralError>
                 <ul>
                     {laravelError.errors.map((error, index) => (
                         <li key={index}>{error.detail}</li>
                     ))}
                 </ul>
-            </AlertDescription>
-        </Alert>
-    );
+            </GeneralError>
+        );
+    }
+
+    if (children) {
+        return <GeneralError>{children}</GeneralError>;
+    }
+
+    return <></>;
 };
 
 export default Error;
